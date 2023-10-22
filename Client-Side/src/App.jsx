9a5439238263cb60e5f0c95103bd754c10fd778e
its-me-ojas <crestfallenwizard@gmail.com>
2023-10-22 18:25:10 +0530

    alignment change

--- Client-Side/src/App.jsx
+++ Client-Side/src/App.jsx
@@ -1,7 +1,12 @@
+import PDFInput from "./Components/PDFInput"
 export default function App() {
   return (
-    <h1 className="text-red-500 font-bold underline">
-      AI Quiz Maker
-    </h1>
+    <>
+      <h1 className="text-red-500 font-bold underline text-5xl mb-5">
+        AI Quiz Maker
+      </h1>
+      <PDFInput className="text-slate-50"></PDFInput>
+    </>
+
   )
 }
